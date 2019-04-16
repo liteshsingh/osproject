299b99f7b1a95417853f53580069bd04b25d4a20
liteshsingh <43039218+liteshsingh@users.noreply.github.com>
2019-04-16 19:54:53 +0530

    Update proj1.cpp

--- proj1.cpp
+++ proj1.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 #include<string.h>
 #include<iostream>
-#include<exception>
-#include<typeinfo>
+#include<unistd.h>
+
 int t = 0;
 
 int br_t =0;
@@ -14,34 +14,21 @@ class Process
   int pr;
   int arr_time;
   int burst_time;
-  int comp_t=0;
-  int wt_t = 0;
+  int comp_t;
+  int wt_t;
+  int ta_t;
 public:
   Process()
   {
   
    }
- void get_attr()
-  {
-  
-    cout<<"Enter Process ID [p(i)]: ";
-    cin>>this->pid;
-    cout<<"Enter Process Priority:";
-    cin>>this->pr;
-    cout<<"Enter Arrival Time:";
-    cin>>this->arr_time;
-    cout<<"Enter Burst Time:";
-    cin>>this->burst_time; 
-     this->comp_t = 0;
-       
-    }
   
   void show()
   {
     
     cout<<this->pid  <<"         " <<this->arr_time <<"               " <<this->pr <<"              " <<this->burst_time <<"\n";
     }
-  friend void Proc_Schedular(Process *p,int n);
+  //friend void Proc_Schedular(Process *p,int n);
  
   
 
@@ -63,12 +50,12 @@ bt[i] = p[i].burst_time;
  while (1) 
 	{ 
 		bool done = true; 
-               if(t >= br_t)
+                if(t >= br_t)
                  break;
               
 		for (int i = 0 ; i < n; i++) 
 		{ 
-		    
+		    int l =0;
                     if(t >= br_t)
                         break;
                
@@ -78,21 +65,54 @@ bt[i] = p[i].burst_time;
                            {
                                       if(p[i].burst_time>0){
                                           	done = false; 
-                                                printf("\n%d is running at %d time",p[i].pid,t);
-                                               
-                                                if(p[i].pr >p[i+1].pr && p[i+1].arr_time <=t+time_q)
+                                                printf("\ni : %d \nwait Process %d is running at %d second\n",i,p[i].pid,t);
+                                               int j =i+1;
+                                                
+                                  while(j<n)
+                                     {
+                                       if(p[j].arr_time>=t+time_q)
+                                                    break;
+                                          else if(p[j].pr>p[i].pr)
+                                               {      int a =0;
+                                                      if(p[j].arr_time - t >= 0){
+                                                   p[i].burst_time-=p[j].arr_time-t;
+                                                            
+                                                           a =p[j].arr_time - t;
+                                                          t = p[j].arr_time;
+                                                           sleep(a/10);}
+                                printf("\nprocess %d runs for %d seconds \nprocess %d preempted by process id : %d\n",p[i].pid,a,p[i].pid,p[j].pid);
+                                                         
+                                                          Process temp =  p[i];
+                                                          p[i] = p[j];
+                                                          p[j] = temp;
+                                                          l++;
+                                                          break;
+                                                          
+                                                               
+                                                    }
+                                            else
+                                                j++;
+                                        }
+                                    if(l==1)
+                                       {i--;
+                                         printf("\ni :%d " ,i );
+                                          continue;
+                                                }
+                                              /*  if(p[i].pr <p[i+1].pr && p[i+1].arr_time <=t+time_q)
                                                          { 
                                                            p[i].burst_time-=p[i+1].arr_time-t;
                                                            int a =p[i+1].arr_time - t;
-                                              printf("\nprocess %d runs for %d \nprocess %d preempted by %d",p[i].pid,a,p[i].pid,p[i+1].pid);
-                                                            t = p[i+1].arr_time;
+                                                           sleep(a/10);
+                                printf("\nprocess %d runs for %d seconds \nprocess %d preempted by process id : %d\n",p[i].pid,a,p[i].pid,p[i+1].pid);
+                                                           t = p[i+1].arr_time;
                                                            continue;
-                                                              }       
-			                    	else if (p[i].burst_time > time_q) 
+                                                              }      */
+			                    	if (p[i].burst_time > time_q) 
 			                        	{ 
 			  		                      
 				                        	t += time_q;
-                                                                printf("\n%d runs for %d",p[i].pid,t);
+                                                                sleep(time_q/10);
+                                                                printf("\n%d runs for %d seconds\n",p[i].pid,time_q);
                                                                 p[i].burst_time -= time_q;
                                                                 
                                                                  
@@ -103,9 +123,11 @@ bt[i] = p[i].burst_time;
 				                          { 
 				                               
 				                                t = t + p[i].burst_time; 
-                                                                printf("\n%d runs for %d",p[i].pid,t);
+                                                                sleep(p[i].burst_time/10);
+                                                                printf("\n%d runs for %d seconds\n",p[i].pid,p[i].burst_time);
 				                          	p[i].burst_time = 0; 
                                                                 p[i].comp_t = t;
+                                                                p[i].ta_t  = p[i].comp_t - p[i].arr_time;
                                                                 p[i].wt_t = p[i].comp_t-bt[i]-p[i].arr_time;
 				                               }            
                                               
@@ -138,32 +160,37 @@ bt[i] = p[i].burst_time;
 
     if(t<br_t)
         t = br_t;
-     Process q[n];
-     for(int j=0;j<n;j++)
-          q[j]=p[j];
+     Process *q = p;
+    // for(int j=0;j<n;j++)
+     //     q[j]=p[j];
    for(int j=0;j<n-1;j++)
     {
      for(int k=0 ;k<n-j-1;k++)
         { 
-          if(q[k].pr>q[k+1].pr)
+          if(q[k].pr<q[k+1].pr)
            {
               Process temp = q[k];
               q[k] = q[k+1];
               q[k+1] = temp;
+              int tmp = bt[k];
+              bt[k] = bt[k+1];
+              bt[k+1] = tmp;
               }
           }
       }
-   cout<<"\nPid  |  Arrival-Time  |  Priority  |  Burst-Time  \n";
-    for(int l =0;l<n;l++)
-     q[l].show();
-  
 
- // second queue scheduling ;
+  cout<<"\nAll Processes are in ready-state now\n";
+  /* cout<<"\nPid  |  Arrival-Time  |  Priority  |  Burst-Time  \n";
+    for(int l =0;l<n;l++)
+     q[l].show();*/
+// ........................................................................start here ................................................................ 
+
+// second queue scheduling ;
 
    while (1) 
 	{ 
 		bool done = true; 
-           printf("\nEntered Second Queue");
+          // printf("\nEntered Second Queue");
               
 		for (int i = 0 ; i < n; i++) 
 		{ 
@@ -175,12 +202,14 @@ bt[i] = p[i].burst_time;
 			           if (q[i].burst_time > 0) 
 			              { 
 			                  	done = false; 
-                                                printf("\n%d is running at %d time",q[i].pid,t);
+                                                printf("\nwait Process %d is running at %d second \n",q[i].pid,t);
                                     
 			                    	if (q[i].burst_time > time_q) 
 			                        	{ 
 			  		
 				                        	t += time_q;
+                                                                sleep(time_q/10);
+                                                                printf("\n%d runs for %d seconds\n",q[i].pid,time_q);
                                                                 q[i].burst_time -= time_q; 
 				                          } 
 
@@ -189,9 +218,12 @@ bt[i] = p[i].burst_time;
 				                          { 
 				
 				                           	t = t + q[i].burst_time; 
+                                                                sleep(q[i].burst_time/10);
+                                                                printf("\n%d runs for %d seconds\n",p[i].pid,q[i].burst_time);
 				                          	q[i].burst_time = 0; 
-                                                                p[i].comp_t = t;
-                                                                p[i].wt_t = p[i].comp_t-bt[i]-p[i].arr_time;
+                                                                q[i].comp_t = t;
+                                                                q[i].ta_t = q[i].comp_t - q[i].arr_time;
+                                                                q[i].wt_t = q[i].comp_t-bt[i]-q[i].arr_time;
 				                               }                
                                               
 			                   }  
@@ -213,11 +245,16 @@ bt[i] = p[i].burst_time;
 		if (done == true) 
 		break; 
 	} 
- 
+ //..................................................................................................upto this ......................................
+  cout<<endl <<"All Processes Executed Successfully \n";
+  cout<<"Thank You for your waiting\n";
+  
+   
+  cout<<"----pid----"   <<"----completion-time----"   <<"----wait-time----" << "----Turn-Around-time----"  <<"\n";
    for(int m=0;m<n;m++)
       {
-         cout<<"pid       "   <<"comp_t       "   <<"wait_t     ";
-         cout<<p[m].pid <<"       "  <<p[m].comp_t <<"       "  <<p[m].wt_t;
+        
+         cout<<"|   " <<p[m].pid <<"    |            "  <<p[m].comp_t <<"       |             "  <<p[m].wt_t <<"    |            "<<p[m].ta_t <<endl;
  
           }
  
@@ -234,13 +271,42 @@ int main()
 
  do{
   int i = 0,n;
-  cout<<"Enter Number of processes you want to schedule";
+  do{
+  cout<<"\nEnter Number of processes you want to schedule : ";
   cin>>n;
+    if(n<= 0)
+   cout<<"\nOOps wrong value , Try Again\n"; 
+   }while(n<=0);
+  
   Process P[n];
   for(;i<n;i++)
    {
-        P[i].get_attr(); 
-        
+             int a ;
+    cout<<"\n Enter Process ID [p(i)] : ";
+       cin>>a;
+       if(a < 0)
+         {printf("\nOOps wrong value ; Try again \n");i--;continue;}
+       P[i].pid =a;
+    
+    cout<<"\n Enter Process Priority :";
+      cin>>a;
+        if(a < 0)
+         {printf("\nOOps wrong value ; Try again \n");i--;continue;}
+       P[i].pr =a ;
+    cout<<"\n Enter Arrival Time :";
+      cin>>a;
+      if(a < 0)
+         {printf("\nOOps wrong value ; Try again \n");i--;continue;}
+   P[i].arr_time =a;
+    cout<<"\n Enter Burst Time :";
+     cin>>a;
+     if(a < 0)
+         {printf("\nOOps wrong value ; Try again \n");i--;continue;}
+   P[i].burst_time =a; 
+     P[i].comp_t = 0;
+     P[i].wt_t =0;
+     P[i].ta_t = 0; 
+        cout<<endl <<i+1 <<" Process added Successfully \n";
       }
 
 for(int i=0;i<n-1;i++)
@@ -258,20 +324,27 @@ for(int i=0;i<n-1;i++)
 
 
 int time_q=0;
-
   
+  do {cout<<"\n Enter the time Quauntum : ";
+  cin>>time_q;
+   if(time_q  <=0)
+    cout<<"\nOOps wrong value ; Try again \n";
+   }while(time_q<=0);
+   
+  system("clear");
+  cout<<"\n Review what you have added \n";
+   
   cout<<"\nPid  |  Arrival-Time  |  Priority  |  Burst-Time  \n";
     for(i=0;i<n;i++){
          P[i].show();  
     }
-    
+   cout<<"\n Time Quantum is : " <<time_q <<endl;
       br_t = P[n-1].arr_time;
       t = P[0].arr_time;
-  cout<<"Enter the time Quauntum : ";
-  cin>>time_q;
- Proc_Schedular(P,n,time_q);
+   //sleep(5);
+   Proc_Schedular(P,n,time_q);
      
-  printf("\ndo you want to run another time :\nEnter: y or n \n");
+   printf("\ndo you want to run another time :\nEnter: y or n \n");
    cin>>ch;
            
   
